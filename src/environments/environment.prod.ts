2f9859951a753eaffe9792f0229608e7fb035f37
Jakub <148567047+Xmausix@users.noreply.github.com>
2026-06-07 22:09:21 +0200

    Add production environment and update .gitignore
    Remove src/environments/ from .gitignore and add src/environments/environment.prod.ts. The new file defines production=true and OpenWeather configuration (base URLs and API key) so a production environment file can be committed and used in builds.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    openWeatherBaseUrl: 'https://api.openweathermap.org/data/2.5',
+    openWeatherGeoUrl: 'https://api.openweathermap.org/geo/1.0',
+    openWeatherApiKey: 'de3483628b64a03646b24fcbcc222d0d',
+};
